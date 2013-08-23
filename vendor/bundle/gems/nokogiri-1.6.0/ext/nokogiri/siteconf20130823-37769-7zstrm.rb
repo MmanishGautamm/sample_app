afa29ec08044cb7a5d44dbcf8aff480e17454efb
Manish Gautam <manish.cse@gmail.com>
2013-08-23 12:45:46 +0530

    Initial Commit

---
+++ vendor/bundle/gems/nokogiri-1.6.0/ext/nokogiri/siteconf20130823-37769-7zstrm.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130823-37769-14ey89j"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
